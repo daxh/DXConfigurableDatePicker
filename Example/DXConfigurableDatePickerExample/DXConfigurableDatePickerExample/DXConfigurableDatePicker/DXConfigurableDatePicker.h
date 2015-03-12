0577dd65c29661a0f66c759f47c703e93766de22
Suprun Denis <suprundenis@gmail.com>
2015-03-12 11:30:15 +0300

    + Starting point for creating new picker

---
+++ Example/DXConfigurableDatePickerExample/DXConfigurableDatePickerExample/DXConfigurableDatePicker/DXConfigurableDatePicker.h
@@ -0,0 +1,13 @@
+//
+//  DXConfigurableDatePicker.h
+//  DXConfigurableDatePickerExample
+//
+//  Created by Denis Suprun on 12/03/15.
+//  Copyright (c) 2015 daxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DXConfigurableDatePicker : UIPickerView
+
+@end
---
+++ Example/DXConfigurableDatePickerExample/DXConfigurableDatePickerExample/DXConfigurableDatePicker/DXConfigurableDatePicker.m
@@ -0,0 +1,21 @@
+//
+//  DXConfigurableDatePicker.m
+//  DXConfigurableDatePickerExample
+//
+//  Created by Denis Suprun on 12/03/15.
+//  Copyright (c) 2015 daxh. All rights reserved.
+//
+
+#import "DXConfigurableDatePicker.h"
+
+@implementation DXConfigurableDatePicker
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
